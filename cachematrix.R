2a5493def7b93329edcee8c4b9e9873a473309c3
dboriani <dboriani@gmail.com>
2014-09-18 10:49:23 -0400

    first commit, with changed comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,12 @@
+## This file contains a function makeCacheMatrix that creates 
+## a cached matrix, and a function cacheSolve that solves 
+## (i.e. calculates the inverse of a square and invertible matrix),
+## The inverse is calculated only if it is not available in the cache.   
+## Course: R Programming - Coursera.
+## Sept. 2014
+
+
+
 ## Put comments here that give an overall description of what your
 ## functions do
 
